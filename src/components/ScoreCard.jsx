6c512b0952d4b8917e81401d1d94ed7b44ecf20f
N-eeraj <neeraj7rajeena@gmail.com>
2024-07-24 01:03:19 +0530

    Added score modal

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,6 +1,27 @@
+// react imports
+import { useContext } from 'react'
+import { Context } from '@components/ContextProvider'
+
+// style imports
+import style from '@styles/score-card.module.scss'
+
 const ScoreCard = () => {
+  const { score } = useContext(Context)
+
   return (
     <>
+      <div className={style.overlay} />
+      <div className={style.modal}>
+        <span className={style.title}>
+          Your Score
+        </span>
+        <strong className={style.score}>
+          {score}
+        </strong>
+        <button className={style.button}>
+          Play Again
+        </button>
+      </div>
     </>
   )
 }
